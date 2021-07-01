c277b17476dbdc2e09be6405e9205f488f639c40
Simone Gaggio <simone.gaggio@libero.it>
2021-07-01 10:31:38 +0200

    MainDeviazione.c fix up

--- MainDeviazione.c
+++ MainDeviazione.c
@@ -6,11 +6,10 @@
 #include <math.h>
 #include "BSTrees.h"
 #include "RedBlack.c"
-#include "AVL.c"
+#include "AVL2.c"
 #define A 1000    //A=numero di operazioni minime sull' albero= Nmin    1000
 #define Emax 0.01 //Errore relativo massimo
 #define BILLION 1000000000L
-
 //...................................................................................................................................................................
 long getResolution()
 {
@@ -23,20 +22,275 @@ long getResolution()
     return (end.tv_nsec - start.tv_nsec);
 }
 //...............................................................................................................................................................................
-int main()
+
+//TEMPI PER UN BST:
+void bst()
 {
 
-    double B = exp((log(1000000) - log(A)) / 99); //1 mln = numero massimo operazioni sull' albero = Nmaxn   1000000
-    double n;                                     //numero di operazioni sull'albero
-    int measurePrec = 100;                          //indice di precisione della misura ~ 100
+    double n = 10000;      //numero di operazioni sull'albero
+    int measurePrec = 100; //indice di precisione della misura ~ 100
 
     double tn = 0; //TEMPO MEDIO MISURATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
     double D = 0;  //DEVIAZIONE STANDARD
 
-    double *destroyTime; //ARRAY CONTENENTE I TEMPI MEDI MISURATI PER ELIMINARE UN ALBERO, AL VARIARE DI n
-    //destroyTime = TreeDestructionTime(tipo di albero);                   //destroyTime[j] = TEMPO MEDIO MISURATO PER ELIMINARE UN ALBERO CREATO CON n=floor(A*B^j) OPERAZIONI
+    //DETERMINO IL NUMERO DI OPERAZIONI DA ESEGURE IN UN ITERAZIONE DEL FOR PIU ESTERNO
+    double time_sum = 0;        //somma dei tempi medi misurati per eseguire n operazioni
+    double vector[measurePrec]; //OGNI ELEMENTO E' LA MISURA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI(ESCLUDIAMO IL TEMPO PER ELIMINARE L'ALBERO)
 
-    //Menu
+    for (int w = 0; w < measurePrec; w++)
+    {
+        /*
+                RIPETIAMO measurePrec VOLTE LA MISURA DEL TEMPO MEDIO PER ESEGUIRE n OPERAZIONI DI RICERCA E INSERIMENTO
+                POI CALCOLIAMO LA MEDIA. QUESTO CI PERMETTE SIA DI AVERE UNA STIMA PIU PRECISA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
+                MA ANCHE DI AVERE A DISPOSIZIONE LA MEDIA PER CALCOLARE LA DEVIAZIONE STANDARD DEL TEMPO MEDIO E AMMORTIZZATO
+                */
+
+        struct timespec start, end;
+
+        long R = getResolution(); //RISOLUZIONE IN NANO-SECONDI
+        int k = 0;
+
+        double tempo = 0;
+        //STIMIAMO IL TEMPO PER ELIMINARE UN ALBERO COSTRUITO CON n OPERAZIONI(salviamo le misure stimate al variare di n in un array di 100 elemeni, destroyTime[])
+        struct node *bstRootsVector[100];
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        do
+        {
+
+            //INIZIALIZZO ROOT DEL MIO ALBERO
+            bstRootsVector[k] = NULL;
+            for (int i = 0; i < n; i++)
+            {
+
+                //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
+                int key = rand();
+                //CERCO K NELL'ALBERO
+                if (find(bstRootsVector[k], key) == NULL)
+                {
+                    bstRootsVector[k] = insert(bstRootsVector[k], key, ""); //SE NON LA TROVO LA INSERISCO
+                }
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            k++;
+
+            tempo = ((end.tv_sec - start.tv_sec) * (double)BILLION) + (end.tv_nsec - start.tv_nsec); //TEMPO IN NANO-SECONDI
+        } while (tempo < ((R / Emax) + R));
+
+        //DEALLOCAZIONE DEGLI ALBERI CREATI NEL doWhile():
+
+        for (int q = 0; q < k; q++)
+        {
+            destroyTree(bstRootsVector[q]);
+        }
+
+        vector[w] = (tempo / k) / n; //TEMPO MEDIO E AMMORTIZZATO PER L'ESECUZIONE DI n OPERAZIONI(senza il tempo di deallocazione dell'albero creato)
+        printf("%i   %lf\n", (int)floor(n), vector[w]);
+    }
+
+    //CALCOLO TEMPO MEDIO E AMMORTIZZAT0 PER L'ESECUZIONE DI n OPERAZIONI, tn
+    for (int z = 0; z < measurePrec; z++)
+    {
+        time_sum += vector[z];
+    }
+    tn = (time_sum / measurePrec); //media dei tempi medi e ammortizzati per l'esecuzione di n operazioni
+
+    //CALCOLO DEVIAZIONE STANDARD TEMPO MEDIO E AMMORTIZZATO PER ESEGUIRE n OPERAZIONI
+    double sommatoria = 0.00; //inizio del calcolo della deviazione
+
+    for (int i = 0; i < measurePrec; i++)
+    {
+        sommatoria = sommatoria + pow((vector[i] - tn), 2);
+    }
+    D = sqrt(sommatoria / measurePrec); //DEVIAZIONE STANDARD
+
+    printf("La deviazione è: %lf\n",D); //Stampa a video la deviazione standard
+}
+
+//...............................................................................................................................................................................
+
+//TEMPI PER UN BST:
+void avlTree()
+{
+
+    double n = 10000;      //numero di operazioni sull'albero
+    int measurePrec = 100; //indice di precisione della misura ~ 100
+
+    double tn = 0; //TEMPO MEDIO MISURATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
+    double D = 0;  //DEVIAZIONE STANDARD
+
+    //DETERMINO IL NUMERO DI OPERAZIONI DA ESEGURE IN UN ITERAZIONE DEL FOR PIU ESTERNO
+    double time_sum = 0;        //somma dei tempi medi misurati per eseguire n operazioni
+    double vector[measurePrec]; //OGNI ELEMENTO E' LA MISURA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI(ESCLUDIAMO IL TEMPO PER ELIMINARE L'ALBERO)
+
+    for (int w = 0; w < measurePrec; w++)
+    {
+        /*
+                RIPETIAMO measurePrec VOLTE LA MISURA DEL TEMPO MEDIO PER ESEGUIRE n OPERAZIONI DI RICERCA E INSERIMENTO
+                POI CALCOLIAMO LA MEDIA. QUESTO CI PERMETTE SIA DI AVERE UNA STIMA PIU PRECISA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
+                MA ANCHE DI AVERE A DISPOSIZIONE LA MEDIA PER CALCOLARE LA DEVIAZIONE STANDARD DEL TEMPO MEDIO E AMMORTIZZATO
+                */
+
+        struct timespec start, end;
+
+        long R = getResolution(); //RISOLUZIONE IN NANO-SECONDI
+        int k = 0;
+
+        double tempo = 0;
+        //STIMIAMO IL TEMPO PER ELIMINARE UN ALBERO COSTRUITO CON n OPERAZIONI(salviamo le misure stimate al variare di n in un array di 100 elemeni, destroyTime[])
+        struct avl_node *avlRootsVector[100];
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        do
+        {
+
+            //INIZIALIZZO ROOT DEL MIO ALBERO
+            avlRootsVector[k] = NULL;
+            for (int i = 0; i < n; i++)
+            {
+
+                //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
+                int key = rand();
+                //CERCO K NELL'ALBERO
+                if (avl_find(avlRootsVector[k], key) == NULL)
+                {
+                    struct avl_node *n = new_avl_node(k, "");
+                    avlRootsVector[k] = avl_insert(avlRootsVector[k], n, NULL, avlRootsVector[k]); //SE NON LA TROVO LA INSERISCO
+                }
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            k++;
+
+            tempo = ((end.tv_sec - start.tv_sec) * (double)BILLION) + (end.tv_nsec - start.tv_nsec); //TEMPO IN NANO-SECONDI
+        } while (tempo < ((R / Emax) + R));
+
+        //DEALLOCAZIONE DEGLI ALBERI CREATI NEL doWhile():
+
+        for (int q = 0; q < k; q++)
+        {
+            avl_destroyTree(avlRootsVector[q]);
+        }
+
+        vector[w] = (tempo / k) / n; //TEMPO MEDIO E AMMORTIZZATO PER L'ESECUZIONE DI n OPERAZIONI(senza il tempo di deallocazione dell'albero creato)
+        printf("%i   %lf\n", (int)floor(n), vector[w]);
+    }
+
+    //CALCOLO TEMPO MEDIO E AMMORTIZZAT0 PER L'ESECUZIONE DI n OPERAZIONI, tn
+    for (int z = 0; z < measurePrec; z++)
+    {
+        time_sum += vector[z];
+    }
+    tn = (time_sum / measurePrec); //media dei tempi medi e ammortizzati per l'esecuzione di n operazioni
+
+    //CALCOLO DEVIAZIONE STANDARD TEMPO MEDIO E AMMORTIZZATO PER ESEGUIRE n OPERAZIONI
+    double sommatoria = 0.00; //inizio del calcolo della deviazione
+
+    for (int i = 0; i < measurePrec; i++)
+    {
+        sommatoria = sommatoria + pow((vector[i] - tn), 2);
+    }
+    D = sqrt(sommatoria / measurePrec); //DEVIAZIONE STANDARD
+
+    printf("La deviazione è: %lf\n",D); //Stampa a video la deviazione standar
+}
+
+//...............................................................................................................................................................................
+
+//TEMPI PER UN RBT:
+void rbt()
+{
+
+    double n = 10000;      //numero di operazioni sull'albero
+    int measurePrec = 100; //indice di precisione della misura ~ 100
+
+    double tn = 0; //TEMPO MEDIO MISURATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
+    double D = 0;  //DEVIAZIONE STANDARD
+
+    double time_sum = 0;        //somma dei tempi medi misurati per eseguire n operazioni
+    double vector[measurePrec]; //OGNI ELEMENTO E' LA MISURA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI(ESCLUDIAMO IL TEMPO PER ELIMINARE L'ALBERO)
+
+    for (int w = 0; w < measurePrec; w++)
+    {
+        /*
+                RIPETIAMO measurePrec VOLTE LA MISURA DEL TEMPO MEDIO PER ESEGUIRE n OPERAZIONI DI RICERCA E INSERIMENTO
+                POI CALCOLIAMO LA MEDIA. QUESTO CI PERMETTE SIA DI AVERE UNA STIMA PIU PRECISA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
+                MA ANCHE DI AVERE A DISPOSIZIONE LA MEDIA PER CALCOLARE LA DEVIAZIONE STANDARD DEL TEMPO MEDIO E AMMORTIZZATO
+                */
+
+        struct timespec start, end;
+
+        long R = getResolution(); //RISOLUZIONE IN NANO-SECONDI
+        int k = 0;
+
+        double tempo = 0;
+        //STIMIAMO IL TEMPO PER ELIMINARE UN ALBERO COSTRUITO CON n OPERAZIONI(salviamo le misure stimate al variare di n in un array di 100 elemeni, destroyTime[])
+        struct RBT *rbtsVector[100];
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        do
+        {
+
+            //INIZIALIZZO ROOT DEL MIO ALBERO
+            rbtsVector[k] = new_red_black_tree();
+            for (int i = 0; i < n; i++)
+            {
+
+                //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
+                int key = rand();
+                //CERCO K NELL'ALBERO
+                if (rb_find(rbtsVector[k], rbtsVector[k]->root, key) == rbtsVector[k]->leaf)
+                {
+                    struct rb_node *n = new_tree_node(key, "");
+                    rb_insert(rbtsVector[k], n, rbtsVector[k]->leaf, rbtsVector[k]->root);
+                }
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            k++;
+
+            tempo = ((end.tv_sec - start.tv_sec) * (double)BILLION) + (end.tv_nsec - start.tv_nsec); //TEMPO IN NANO-SECONDI
+        } while (tempo < ((R / Emax) + R));
+
+        //DEALLOCAZIONE DEGLI ALBERI CREATI NEL doWhile():
+
+        for (int q = 0; q < k; q++)
+        {
+
+            rb_destroyTree(rbtsVector[q], rbtsVector[q]->root); //deallocazione dell'albero
+            free(rbtsVector[q]->leaf);
+            free(rbtsVector[q]);
+        }
+
+        vector[w] = (tempo / k) / n; //TEMPO MEDIO E AMMORTIZZATO PER L'ESECUZIONE DI n OPERAZIONI(senza il tempo di deallocazione dell'albero creato)
+        printf("%i   %lf\n", (int)floor(n), vector[w]);
+    }
+
+    //CALCOLO TEMPO MEDIO E AMMORTIZZAT0 PER L'ESECUZIONE DI n OPERAZIONI, tn
+    for (int z = 0; z < measurePrec; z++)
+    {
+        time_sum += vector[z];
+    }
+    tn = (time_sum / measurePrec); //media dei tempi medi e ammortizzati per l'esecuzione di n operazioni
+
+    //CALCOLO DEVIAZIONE STANDARD TEMPO MEDIO E AMMORTIZZATO PER ESEGUIRE n OPERAZIONI
+    double sommatoria = 0.00; //inizio del calcolo della deviazione
+
+    for (int i = 0; i < measurePrec; i++)
+    {
+        sommatoria = sommatoria + pow((vector[i] - tn), 2);
+    }
+    D = sqrt(sommatoria / measurePrec); //DEVIAZIONE STANDARD
+
+    printf("La deviazione è: %lf\n",D); //Stampa a video la deviazione standar
+}
+//..................................................................................................................................................................................................................
+int main()
+{
     int scelta;
     printf("Scegli il tipo di albero:\n");
     printf("1-BST\n");
@@ -45,143 +299,24 @@ int main()
     printf("Albero: ");
     scanf("%d", &scelta);
 
-        n = 1000;          //DETERMINO IL NUMERO DI OPERAZIONI DA ESEGURE IN UN ITERAZIONE DEL FOR PIU ESTERNO
-        double time_sum = 0;        //somma dei tempi medi misurati per eseguire n operazioni
-        double vector[measurePrec]; //OGNI ELEMENTO E' LA MISURA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI(ESCLUDIAMO IL TEMPO PER ELIMINARE L'ALBERO)
+    switch (scelta)
+    {
 
-        for (int w = 0; w < measurePrec; w++)
-        {
-            /*
-                RIPETIAMO measurePrec VOLTE LA MISURA DEL TEMPO MEDIO PER ESEGUIRE n OPERAZIONI DI RICERCA E INSERIMENTO
-                POI CALCOLIAMO LA MEDIA. QUESTO CI PERMETTE SIA DI AVERE UNA STIMA PIU PRECISA DEL TEMPO IMPIEGATO PER ESEGUIRE n OPERAZIONI SULL ALBERO
-                MA ANCHE DI AVERE A DISPOSIZIONE LA MEDIA PER CALCOLARE LA DEVIAZIONE STANDARD DEL TEMPO MEDIO E AMMORTIZZATO
-                */
+    case 1:
+        bst();
+        break; //end case1
 
-            struct timespec start, end;
+    case 2:
+        avlTree();
+        break; //end case2
 
-            long R = getResolution(); //RISOLUZIONE IN NANO-SECONDI
+    case 3:
+        rbt();
+        break; //end case3
 
-            int k = 0;
-
-            double tempo = 0;
-            //STIMIAMO IL TEMPO PER ELIMINARE UN ALBERO COSTRUITO CON n OPERAZIONI(salviamo le misure stimate al variare di n in un array di 100 elemeni, destroyTime[])
-            clock_gettime(CLOCK_MONOTONIC, &start);
-
-            do
-            {
-                struct node *bst_root = NULL;
-                struct avl_node *avl_root = NULL;
-                struct RBT *t = NULL; 
-                
-                switch (scelta)
-                {
-
-                  case 1:
-
-                    //INIZIALIZZO ROOT DEL MIO ALBERO
-                    
-
-                    for (int i = 0; i < n; i++)
-                    {
-                        //printf("lol\n");
-                        //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
-                        int k = rand();
-                        //CERCO K NELL'ALBERO
-                        if (find(bst_root, k) == NULL)
-                        {
-                            bst_root = insert(bst_root, k, ""); //SE NON LA TROVO LA INSERISCO
-                        }
-                    }
-
-                    clock_gettime(CLOCK_MONOTONIC, &end);
-                    //DESTROY TREE: DEVO ELIMINARE L'ALBERO CHE HO CREATO
-                    destroyTree(bst_root);
-
-                    break; //end case1
-
-                  case 2:
-                    //INIZIALIZZO ROOT DEL MIO ALBERO
-                    
-
-                    for (int i = 0; i < n; i++)
-                    {
-                        
-                        //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
-                        int k = rand();
-                        //CERCO K NELL'ALBERO
-                        if (avl_find(avl_root, k) == NULL)
-                        {
-                            avl_root = avl_insert(avl_root, k, ""); //SE NON LA TROVO LA INSERISCO
-                        }
-                    }
-
-                    clock_gettime(CLOCK_MONOTONIC, &end);
-                    //DESTROY TREE: DEVO ELIMINARE L'ALBERO CHE HO CREATO
-                    avl_destroyTree(avl_root);
-
-                    break; //end case2
-
-                  case 3:
-
-                    
-                    t = new_red_black_tree();
-                    for (int i = 0; i < n; i++)
-                    {
-                        //printf("lol\n");
-                        //GENERO LA CHIAVE K IN MANIERA PSEUDO-CASUALE
-                        int k = rand();
-                        //CERCO K NELL'ALBERO
-                        if (rb_find(t, t->root,k) == NULL)
-                        {
-                            struct rb_node *n = new_tree_node(k, "");
-                            rb_insert(t, n);
-                        }
-                    }
-                    clock_gettime(CLOCK_MONOTONIC, &end);
-                    rb_destroyTree(t, t->root); //deallocazione dell'albero
-                    free(t->leaf);
-                    free(t);
-
-                    break; //end case3
-                  
-
-
-                   default:
-                        printf("Valore non valido.\n");
-                        break;
-                    
-
-                 
-                }
-                
-                k++;
-                tempo = ((end.tv_sec - start.tv_sec) *(double)BILLION)  + (end.tv_nsec - start.tv_nsec); //TEMPO IN NANO-SECONDI
-
-            } while (tempo < ((R / Emax) + R));
-
-            vector[w] = (((tempo / k)- destroyTime[24]) / n); //TEMPO MEDIO E AMMORTIZZATO PER L'ESECUZIONE DI n OPERAZIONI
-                                                            //NOTA CAMBIA 24 NELL INDICE j CHE TI DA 10000
-            printf("%i   %lf\n", (int)floor(n), vector[w]);
-        }
-
-        //CALCOLO TEMPO MEDIO E AMMORTIZZAT0 PER L'ESECUZIONE DI n OPERAZIONI, tn
-        for (int z = 0; z < measurePrec; z++)
-        {
-            time_sum += vector[z];
-        }
-        tn = (time_sum / measurePrec); //MEDIA DEI TEMPI MEDI E AMMORTIZZATI PER L'ESECUZIONE DI n OPERAZIONI(VALORE CHE RESTITUIAMO)
-
-        //CALCOLO DEVIAZIONE STANDARD TEMPO MEDIO E AMMORTIZZATO PER ESEGUIRE n OPERAZIONI
-        double sommatoria = 0.00; //inizio del calcolo della deviazione
-
-        for (int i = 0; i < measurePrec; i++)
-        {
-            sommatoria = sommatoria + pow((vector[i] - tn), 2);
-        }
-        D = sqrt(sommatoria / measurePrec); //DEVIAZIONE STANDARD
-        printf("la media è: %lf\n", tn);
-        printf("la deviazione standard è: %lf\n", D); //stampa a video la combinata della lunghezza della stringa e del tempo impiegato a processarla
-    
-
+    default:
+        printf("Valore non valido.\n");
+        break;
+    }
     return 0;
 }
